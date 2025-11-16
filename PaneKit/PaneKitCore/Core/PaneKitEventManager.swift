decb423ac26c439c936fa8e28181c9a9eff16ad8
saschaweiss <info@saschaweiss.com>
2025-11-16 17:58:47 +0100

    Auto batch update (2025-11-16 17:58:47)

--- PaneKit/PaneKitCore/Core/PaneKitEventManager.swift
+++ PaneKit/PaneKitCore/Core/PaneKitEventManager.swift
@@ -13,6 +13,8 @@ final class PaneKitEventManager {
     private var isDragging: Bool = false
     private var lastStableID: String?
     private var lastDraggedStableID: String?
+    private var lastFrameDuringDrag: CGRect?
+    private var lastScreenDuringDrag: NSScreen?
     private static var pendingWindowChanges: [String: (frame: CGRect, screen: NSScreen, lastUpdate: Date)] = [:]
     private static let moveResizeDebounceInterval: TimeInterval = 0.25
     
