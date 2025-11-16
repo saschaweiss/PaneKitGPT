3e3863891ef40388ca6c98b5e6da27b487560103
saschaweiss <info@saschaweiss.com>
2025-11-16 03:51:17 +0100

    Auto batch update (2025-11-16 03:51:17)

--- PaneKit/PaneKitCore/Core/PaneKitManager.swift
+++ PaneKit/PaneKitCore/Core/PaneKitManager.swift
@@ -93,7 +93,7 @@ public final class PaneKitManager: Sendable {
     public func stop() {
         eventManager.stop()
         cache.clear()
-        PaneKitEventManager.stopRecoveryTimer()
+        PaneKitEventManager.stopRecoveryTimer(<#T##self: PaneKitEventManager##PaneKitEventManager#>)
         PaneKitEventManager.
         isRunning = false
         print("🛑 PaneKitManager gestoppt.")
