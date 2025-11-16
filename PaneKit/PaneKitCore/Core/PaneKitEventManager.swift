e70b74d862be835e913e4513496e24c781465964
saschaweiss <info@saschaweiss.com>
2025-11-16 03:54:27 +0100

    Auto batch update (2025-11-16 03:54:27)

--- PaneKit/PaneKitCore/Core/PaneKitEventManager.swift
+++ PaneKit/PaneKitCore/Core/PaneKitEventManager.swift
@@ -65,7 +65,7 @@ extension PaneKitEventManager {
         observers[app.processIdentifier] = observer
             
         let notifications = [
-            AXNotify.moved.raw,
+            AXNotify.moved.string,
             kAXResizedNotification,
             kAXFocusedWindowChangedNotification,
             kAXCreatedNotification,
